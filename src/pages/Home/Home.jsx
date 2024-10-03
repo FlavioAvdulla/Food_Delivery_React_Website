b78d9ceb9d302542d23e83cb706e1d343eced197
FlavioAvdulla <a.flavio4366@gmail.com>
2024-10-03 23:38:04 +0200

    website update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <ExploreMenu category = {category} setCategory = {setCategory}/>
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
