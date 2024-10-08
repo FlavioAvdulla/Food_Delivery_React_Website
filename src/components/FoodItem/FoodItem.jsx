d8adef12a887e00140b11f4b8527634bdb234015
FlavioAvdulla <a.flavio4366@gmail.com>
2024-10-08 23:14:16 +0200

    website

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
-import './FoodItem.css';
-import { assets } from '../../assets/assets';
+import React, { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
-    <div className='food-item'>
+    <div className="food-item">
       <div className="food-item-img-container">
-        <img className='food-item-image' src={image} alt={name} />
-        {!itemCount ? (
-          <img className='add' onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_white} alt="Add" />
+        <img className="food-item-image" src={image} alt={name} />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt="Add"
+          />
         ) : (
-          <div className='food-item-counter'>
-            <img onClick={() => setItemCount(prev => prev - 1)} src={assets.remove_icon_red} alt="Remove" />
-            <p>{itemCount}</p>
-            <img onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_green} alt="Add" />
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt="Remove"
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt="Add"
+            />
           </div>
         )}
       </div>
