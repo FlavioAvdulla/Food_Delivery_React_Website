1315e876dce87c77490d5913421efa94a68f8713
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-10 22:24:24 +0200

    web_upd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=> setMenu("home")} className={menu==="home"?"active":""}>home</li>
-        <li onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-        <li onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=> setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+        <Link onClick={()=> setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+        <a ahref='' onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+        <a ahref='' onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+        <a ahref='' onClick={()=> setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
