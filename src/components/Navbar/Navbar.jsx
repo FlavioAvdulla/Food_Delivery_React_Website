211419d8f30b3a5a466edc2f79fbbf9987814ab5
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-24 10:57:34 +0200

    web

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,43 +6,58 @@ import { StoreContext } from "../context/StoreContext";
 
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
-
   const { getTotalCartAmount } = useContext(StoreContext);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [iconColor, setIconColor] = useState(false); // State for icon color
+
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+    setIconColor(!iconColor); // Toggle icon color state
+  };
 
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <Link to="/">
-      <img src="src/assets/tomato_logo_orange.svg" alt="Tomato Logo" />
+        <img src="src/assets/tomato_logo_orange.svg" alt="Tomato Logo" />
       </Link>
-      <ul className="navbar-menu">
-        <Link
-          to="/"
-          onClick={() => setMenu("home")}
-          className={menu === "home" ? "active" : ""}
-        >
-          home
-        </Link>
-        <a
-          href="#explore-menu"
-          onClick={() => setMenu("menu")}
-          className={menu === "menu" ? "active" : ""}
-        >
-          menu
-        </a>
-        <a
-          href="#app-download"
-          onClick={() => setMenu("mobile-app")}
-          className={menu === "mobile-app" ? "active" : ""}
-        >
-          mobile-app
-        </a>
-        <a
-          href="#footer"
-          onClick={() => setMenu("contact-us")}
-          className={menu === "contact-us" ? "active" : ""}
-        >
-          contact us
-        </a>
+      <ul className={`navbar-menu ${mobileMenu ? "open" : ""}`}>
+        {/* Add 'open' class when mobile menu is active */}
+        <li>
+          <Link
+            to="/"
+            onClick={() => setMenu("home")}
+            className={menu === "home" ? "active" : ""}
+          >
+            HOME
+          </Link>
+        </li>
+        <li>
+          <a
+            href="#explore-menu"
+            onClick={() => setMenu("menu")}
+            className={menu === "menu" ? "active" : ""}
+          >
+            MENU
+          </a>
+        </li>
+        <li>
+          <a
+            href="#app-download"
+            onClick={() => setMenu("mobile-app")}
+            className={menu === "mobile-app" ? "active" : ""}
+          >
+            MOBILE APP
+          </a>
+        </li>
+        <li>
+          <a
+            href="#footer"
+            onClick={() => setMenu("contact-us")}
+            className={menu === "contact-us" ? "active" : ""}
+          >
+            CONTACT US
+          </a>
+        </li>
       </ul>
       <div className="navbar-right">
         <i className="fa-solid fa-magnifying-glass"></i>
@@ -54,7 +69,12 @@ const Navbar = ({ setShowLogin }) => {
         </div>
         <button onClick={() => setShowLogin(true)}>SIGN IN</button>
       </div>
-    </div>
+      <i
+        className={`fa-solid fa-bars ${iconColor ? "active-icon" : ""}`}
+        alt=""
+        onClick={toggleMenu}
+      ></i>{" "}
+    </nav>
   );
 };
 
