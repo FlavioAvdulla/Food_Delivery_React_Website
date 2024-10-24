78337877cecfa37425d2e35a8fb8364e290aabe3
Flavio Avdulla <a.flavio4366@gmail.com>
2024-10-24 11:58:15 +0200

    web

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,72 +8,73 @@ const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   const { getTotalCartAmount } = useContext(StoreContext);
   const [mobileMenu, setMobileMenu] = useState(false);
-  const [iconColor, setIconColor] = useState(false); // State for icon color
+  const [iconColor, setIconColor] = useState(false);
 
   const toggleMenu = () => {
     setMobileMenu(!mobileMenu);
-    setIconColor(!iconColor); // Toggle icon color state
+    setIconColor(!iconColor);
   };
 
   return (
-    <nav className="navbar">
-      <Link to="/">
-        <img src="src/assets/tomato_logo_orange.svg" alt="Tomato Logo" />
-      </Link>
-      <ul className={`navbar-menu ${mobileMenu ? "open" : ""}`}>
-        {/* Add 'open' class when mobile menu is active */}
-        <li>
-          <Link
-            to="/"
-            onClick={() => setMenu("home")}
-            className={menu === "home" ? "active" : ""}
-          >
-            HOME
-          </Link>
-        </li>
-        <li>
-          <a
-            href="#explore-menu"
-            onClick={() => setMenu("menu")}
-            className={menu === "menu" ? "active" : ""}
-          >
-            MENU
-          </a>
-        </li>
-        <li>
-          <a
-            href="#app-download"
-            onClick={() => setMenu("mobile-app")}
-            className={menu === "mobile-app" ? "active" : ""}
-          >
-            MOBILE APP
-          </a>
-        </li>
-        <li>
-          <a
-            href="#footer"
-            onClick={() => setMenu("contact-us")}
-            className={menu === "contact-us" ? "active" : ""}
-          >
-            CONTACT US
-          </a>
-        </li>
-      </ul>
-      <div className="navbar-right">
-        <i className="fa-solid fa-magnifying-glass"></i>
-        <div className="navbar-search-icon">
-          <Link to="/cart">
-            <i className="fa-solid fa-cart-shopping"></i>
-          </Link>
-          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+    <nav className="navbar-background">
+      <nav className="navbar">
+        <Link to="/">
+          <img src="src/assets/tomato_logo_orange.svg" alt="Tomato Logo" />
+        </Link>
+        <ul className={`navbar-menu ${mobileMenu ? "open" : ""}`}>
+          <li>
+            <Link
+              to="/"
+              onClick={() => setMenu("home")}
+              className={menu === "home" ? "active" : ""}
+            >
+              HOME
+            </Link>
+          </li>
+          <li>
+            <a
+              href="#explore-menu"
+              onClick={() => setMenu("menu")}
+              className={menu === "menu" ? "active" : ""}
+            >
+              MENU
+            </a>
+          </li>
+          <li>
+            <a
+              href="#app-download"
+              onClick={() => setMenu("mobile-app")}
+              className={menu === "mobile-app" ? "active" : ""}
+            >
+              MOBILE APP
+            </a>
+          </li>
+          <li>
+            <a
+              href="#footer"
+              onClick={() => setMenu("contact-us")}
+              className={menu === "contact-us" ? "active" : ""}
+            >
+              CONTACT US
+            </a>
+          </li>
+        </ul>
+        <div className="navbar-right">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <div className="navbar-search-icon">
+            <Link to="/cart">
+              <i className="fa-solid fa-cart-shopping"></i>
+            </Link>
+            <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+          </div>
+          <button onClick={() => setShowLogin(true)}>SIGN IN</button>
         </div>
-        <button onClick={() => setShowLogin(true)}>SIGN IN</button>
-      </div>
-      <i
-        className={`fa-solid fa-bars ${iconColor ? "active-icon" : ""}`}
-        alt=""
-        onClick={toggleMenu}
-      ></i>{" "}
+        <i
+          className={`fa-solid fa-bars ${iconColor ? "active-icon" : ""}`}
+          alt=""
+          onClick={toggleMenu}
+        ></i>{" "}
+      </nav>
     </nav>
   );
 };
