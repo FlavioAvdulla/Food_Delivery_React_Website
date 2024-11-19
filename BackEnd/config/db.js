b263c5905dbd6dd4909c2f2e3da0ff76a6260890
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-19 23:34:56 +0100

    web

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://aflavio4366:F!@#264v10@cluster0.18o0f.mongodb.net/Food_Delivery_React_Website').then(()=>console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://aflavio4366:43664366@cluster0.ifcq4.mongodb.net/Food_Delivery_React_Website').then(() => console.log("DB Connected"))
 }
