55b077d2d11c2fb4bf73750717a64c9fa2b68ab4
Flavio Avdulla <a.flavio4366@gmail.com>
2024-11-20 23:14:32 +0100

    web

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -3,6 +3,9 @@ import "./Add.css";
 import { assets } from "../../assets/assets";
 
 const Add = () => {
+
+  const [image, setImage] = useState(false);
+
   return (
     <div className="add">
       <form className="flex-col">
@@ -11,7 +14,7 @@ const Add = () => {
           <label htmlFor="image">
             <img src={assets.upload_area} alt="" />
           </label>
-          <input type="file" id="image" hidden required />
+          <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" hidden required />
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
